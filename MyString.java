d765347dc763b640f90a3cbbb42c481eb183608c
hugo boss <ohad.shoshanilevi@post.runi.ac.il>
2024-12-09 20:43:27 +0200

    done

--- MyString.java
+++ MyString.java
@@ -24,14 +24,6 @@ public class MyString {
      */
     public static int countChar(String str, char ch) {
         int counter = 0;
-        if (str.isEmpty()) {
-            return 0;
-        }
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ch) {
-                counter++;
-            }
-        }
         return counter;
     }
 
